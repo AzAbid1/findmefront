dad1d8ca411a684c4d87b0ed87b6efecf203b6cc
AzAbid1 <73388494+AzAbid1@users.noreply.github.com>
2022-10-22 15:45:45 +0100

    google fonts added

---
+++ android/app/src/main/kotlin/com/example/findmefront/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.findmefront
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Constants {
+  static Color lightPrimary = Color(0xfff3f4f9);
+  static Color darkPrimary = Color(0xff2B2B2B);
+
+  static Color lightAccent = Color(0xff886EE4);
+
+  static Color darkAccent = Color(0xff886EE4);
+
+  static Color lightBG = Color(0xfff3f4f9);
+  static Color darkBG = Color(0xff2B2B2B);
+
+  static ThemeData darkTheme = ThemeData(
+    iconTheme: const IconThemeData(color: Colors.white),
+    colorScheme: ColorScheme.fromSwatch(
+      accentColor: darkAccent,
+    ).copyWith(
+      secondary: darkAccent,
+      brightness: Brightness.dark,
+    ),
+    backgroundColor: darkBG,
+    primaryColor: darkPrimary,
+    scaffoldBackgroundColor: darkBG,
+    textSelectionTheme: TextSelectionThemeData(
+      cursorColor: darkAccent,
+    ),
+    bottomAppBarTheme: BottomAppBarTheme(
+      elevation: 0,
+      color: darkBG,
+    ),
+    appBarTheme: AppBarTheme(
+      elevation: 0.0,
+      backgroundColor: darkBG,
+      iconTheme: const IconThemeData(color: Colors.white),
+      toolbarTextStyle: GoogleFonts.nunito(
+        color: Colors.white,
+        fontSize: 20.0,
+        fontWeight: FontWeight.w700,
+      ),
+      titleTextStyle: GoogleFonts.nunito(
+        color: Colors.white,
+        fontSize: 20.0,
+        fontWeight: FontWeight.w700,
+      ),
+    ),
+  );
+}
