1d5c6607e0e204c52c2381eb25a6dcca6e1695ae
AzAbid1 <73388494+AzAbid1@users.noreply.github.com>
2022-10-25 20:20:02 +0100

    cleaned main.dart + added style to create post

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'package:findmefront/utils/constants.dart';
-import 'screens/feed_screen.dart';
+import 'screens/home_screen.dart';
 import 'screens/create_post_screen.dart';
 
 void main() {
@@ -17,109 +16,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: Constants.themeData,
-      home: const MyHomePage(),
+      home: const HomeScreen(),
       routes: {
         CreatePost.routeName: (ctx) => CreatePost(),
       },
       debugShowCheckedModeBanner: false,
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final List screens = [
-    {'title': 'Home', 'icon': Icons.home, 'screen': FeedScreen()},
-    {
-      'title': 'Notification',
-      'icon': Icons.notifications,
-      'screen': Text('Notification')
-    },
-    {'title': 'Messages', 'icon': Icons.chat, 'screen': Text('messagaes')},
-  ];
-
-  int currentIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    // This code serves to block horizontal orientation (Aziz)
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitUp,
-    ]);
-
-    var bottomAppBar = BottomNavigationBar(
-      backgroundColor: Colors.transparent,
-      elevation: 0,
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      unselectedItemColor: Colors.white70,
-      selectedItemColor: Colors.white,
-      currentIndex: currentIndex,
-      onTap: (index) {
-        setState(() {
-          currentIndex = index;
-        });
-      },
-      items: [
-        for (Map item in screens)
-          BottomNavigationBarItem(
-              icon: Icon(item['icon']), label: item['title']),
-      ],
-    );
-
-    final appBar = AppBar(
-      elevation: 0,
-      leading: TextButton(
-        onPressed: null,
-        child: CircleAvatar(
-          backgroundImage: AssetImage('assets/images/profile_default.jpg'),
-        ),
-      ),
-      title: Image.asset(
-        'assets/images/logo.png',
-        fit: BoxFit.fill,
-        height: 35,
-      ),
-      centerTitle: true,
-      //actionsIconTheme: IconThemeData(color: Theme.of(context).colorScheme.primary),
-      actions: [
-        IconButton(
-          onPressed: null,
-          icon: Icon(Icons.search),
-        )
-      ],
-    );
-
-    return Scaffold(
-        extendBody: true,
-        appBar: appBar,
-        bottomNavigationBar: Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.primary,
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              ),
-            ),
-            child: bottomAppBar),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          onPressed: () {
-            Navigator.of(context).pushNamed(CreatePost.routeName);
-          } ,
-          child:
-              Icon(Icons.add, color: Theme.of(context).colorScheme.secondary),
-        ),
-        // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        body: screens[currentIndex]['screen'],
-
-        );
   }
 }
--- lib/screens/create_post_screen.dart
+++ lib/screens/create_post_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class CreatePost extends StatefulWidget {
   static const routeName = '/create-post';
+
   const CreatePost({Key? key}) : super(key: key);
 
   @override
@@ -12,7 +13,16 @@ class _CreatePostState extends State<CreatePost> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Create Post'),),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.close, color: Theme.of(context).colorScheme.primary),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: const Text('Create Post'),
+        actions: [
+          ElevatedButton(onPressed: null, child: Text('POST'))
+        ],
+      ),
       body: Text('Form here'),
     );
   }
