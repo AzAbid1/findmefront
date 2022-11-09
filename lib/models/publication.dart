68a0f2c2389ee7e4607ce2938a0c4f1b5f6b3b4e
wissem11037 <wissemcheour@gmail.com>
2022-11-10 00:24:51 +0100

    added services,model template and consuming

---
+++ lib/models/publication.dart
@@ -0,0 +1,65 @@
+// To parse this JSON data, do
+//
+//     final publication = publicationFromJson(jsonString);
+
+import 'dart:convert';
+
+List<Publication> publicationFromJson(String str) => List<Publication>.from(json.decode(str).map((x) => Publication.fromJson(x)));
+
+String publicationToJson(List<Publication> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Publication {
+  Publication({
+    required this.id,
+    required this.titre,
+    required this.description,
+    required this.image,
+    required this.nbVue,
+    required this.nbVote,
+    required this.categorieId,
+    required this.villeId,
+    required this.userId,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  int id;
+  String titre;
+  String description;
+  String image;
+  int nbVue;
+  int nbVote;
+  int categorieId;
+  int villeId;
+  int userId;
+  DateTime createdAt;
+  DateTime updatedAt;
+
+  factory Publication.fromJson(Map<String, dynamic> json) => Publication(
+    id: json["id"],
+    titre: json["titre"],
+    description: json["description"],
+    image: json["image"],
+    nbVue: json["nbVue"],
+    nbVote: json["nbVote"],
+    categorieId: json["categorie_id"],
+    villeId: json["ville_id"],
+    userId: json["user_id"],
+    createdAt: DateTime.parse(json["created_at"]),
+    updatedAt: DateTime.parse(json["updated_at"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "titre": titre,
+    "description": description,
+    "image": image,
+    "nbVue": nbVue,
+    "nbVote": nbVote,
+    "categorie_id": categorieId,
+    "ville_id": villeId,
+    "user_id": userId,
+    "created_at": createdAt.toIso8601String(),
+    "updated_at": updatedAt.toIso8601String(),
+  };
+}
--- lib/screens/create_post_screen.dart
+++ lib/screens/create_post_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../models/publication.dart';
+import '../services/remote_service.dart';
+
 class CreatePost extends StatefulWidget {
   static const routeName = '/create-post';
   const CreatePost({Key? key}) : super(key: key);
@@ -9,11 +12,40 @@ class CreatePost extends StatefulWidget {
 }
 
 class _CreatePostState extends State<CreatePost> {
+  List<Publication>? publications;
+  var isLoaded=false;
+  @override
+  void initState(){
+    super.initState();
+    //fetch date from api
+    getData();
+  }
+  getData() async{
+    publications=  await RemoteService().getPublications();
+    if (publications!=null){
+      setState(() {
+        isLoaded=true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Create Post'),),
-      body: Text('Form here'),
+      body: Visibility(
+        visible: isLoaded,
+        replacement: const  Center(
+            child:CircularProgressIndicator()),
+        child: ListView.builder(
+          itemCount: publications?.length,
+          itemBuilder:(context,index) {
+            return Container(
+              child:Text(publications![index].titre,style:TextStyle(fontSize: 24)),
+          );
+        },
+        ),
+      ),
     );
   }
 }
